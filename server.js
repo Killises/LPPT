b2593b8f383b1f7a82ce6b98cfdf5707e9217291
Ulises Cabañas Manrrique <ulicama@hotmail.com>
2025-09-22 14:53:24 -0600

    preparacion para cpanel

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
-
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
-
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+ 
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+ 
 app.prepare().then(() => {
   createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, (err) => {
-    if (err) throw err;
-    console.log("🚀 Server running on port", process.env.PORT || 3000);
-  });
-});
-gi
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+ 
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})
