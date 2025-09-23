f17aff9080a9984f943abfb3719ca716ec5691fb
Ulises Cabañas Manrrique <ulicama@hotmail.com>
2025-09-22 18:10:21 -0600

    metadataBase

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,9 +3,12 @@ import { Montserrat } from 'next/font/google'
 import { ThemeProvider } from '../context/ThemeProvider';
 
 
+
 const montserrat = Montserrat({ weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'], subsets: ['latin'] })
 
 export const metadata = {
+  metadataBase: new URL("https://www.protecciontotal.pro/"),
+  canonical: '/',
   title: "Protección Total .Pro",
   description: "Descubre Protección Total, tu aliado en seguridad. Instalamos alarmas de seguridad, cámaras y control de accesos en Querétaro en menos de 24 h. Protege tu hogar o negocio con tecnología avanzada.",
   icons: {
@@ -19,7 +22,7 @@ export const metadata = {
     siteName: "Protección Total .Pro",
     images: [
       {
-        url: "/images/OGLogo.png",
+        url: "/srv/OGLogo.png",
         width: 1200,
         height: 630,
         alt: "Protección Total .Pro",
@@ -33,17 +36,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es" className={montserrat.className}>
-      <head>
-        {/* <!-- Google tag (gtag.js) --> */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-QXNKQF79VR"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-
-          gtag('config', 'G-QXNKQF79VR');
-        </script>
-      </head>
       <body>
         <ThemeProvider>
           {children}
