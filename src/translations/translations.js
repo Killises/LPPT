d992ce14c875676d26f9041f7c1fbc8c952622b0
Ulises Cabañas Manrrique <ulicama@hotmail.com>
2025-10-07 09:00:26 -0600

    cambio a imagenes .webp

--- src/translations/translations.js
+++ src/translations/translations.js
@@ -31,12 +31,12 @@ export const translations = {
         {
           title: "Alarmas inteligentes",
           desc: "Detección precisa de intrusos con sensores y alertas en tiempo real.",
-          srcLight: "/srv/DevicesBlack.png",
-          srcDark: "/srv/DevicesWhite.png"
+          srcLight: "/srv/DevicesBlack.webp",
+          srcDark: "/srv/DevicesWhite.webp"
         },
-        { title: "Videovigilancia HD", desc: "Imágenes nítidas, visión nocturna y acceso remoto desde cualquier lugar.", src: '/srv/Bulletcam.png' },
+        { title: "Videovigilancia HD", desc: "Imágenes nítidas, visión nocturna y acceso remoto desde cualquier lugar.", src: '/srv/Bulletcam.webp' },
         { title: "Alertas instantáneas", desc: "Notificaciones al instante ante cualquier actividad sospechosa.", src: "/srv/alert.gif" },
-        { title: "Control de acceso", desc: "Administra quién entra y sale con soluciones seguras y sencillas.", src: "/srv/acces.jpg" }
+        { title: "Control de acceso", desc: "Administra quién entra y sale con soluciones seguras y sencillas.", src: "/srv/acces.webp" }
       ],
       more: "Conoce más"
     },
@@ -58,8 +58,8 @@ export const translations = {
         "Personalización según tus necesidades",
         "Soporte técnico continuo"
       ],
-      srcLight: "/images/DistB.png",
-      srcDark: "/images/Dist.png"
+      srcLight: "/images/DistB.webp",
+      srcDark: "/images/Dist.webp"
     },
     testimonials: {
       title: "Testimonios",
@@ -68,12 +68,12 @@ export const translations = {
         {
           author: "Carlos M.",
           text: "Con Ajax me siento tranquilo, puedo ver mi negocio desde el celular.",
-          src: "/inst/anexo4.jpg"
+          src: "/inst/anexo4.webp"
         },
         {
           author: "Laura G.",
           text: "Instalaron en el mismo día, ¡excelente servicio!",
-          src: "/inst/anexo1.jpg"
+          src: "/inst/anexo1.webp"
         }
       ]
     },
@@ -97,25 +97,25 @@ export const translations = {
         {
           title: "Red Dot Winner 2024",
           desc: "Ajax Systems gana su primer premio Red Dot Award por tres productos de seguridad y de domótica",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2Freddot_switch_ab76b6f4e3%401.png&1715325401",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2Freddot_switch_ab76b6f4e3%401.webp&1715325401",
           href: "https://ajax.systems/es/blog/ajax-systems-won-red-dot-award/",
         },
         {
           title: "Security Awards 2024",
           desc: "Ajax Systems gana el premio Security Awards 2024 como Mejor fabricante de equipamiento de seguridad domótica",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FCorporate_Vision_2024_preview_2x_462e208450%401.jpg&1714866025",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FCorporate_Vision_2024_preview_2x_462e208450%401.webp&1714866025",
           href: "https://ajax.systems/es/blog/security-awards-2024/",
         },
         {
           title: "GIT Security Award 2024",
           desc: "FireProtect 2 reconocido con el honorable premio GIT Security Award 2024",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FWGIT_Security_awards_2023_preview_2x_7a943fadfd%401.png&1714864288",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FWGIT_Security_awards_2023_preview_2x_7a943fadfd%401.webp&1714864288",
           href: "https://ajax.systems/es/blog/fireprotect-2-security-award-2024/",
         },
         {
           title: "Australian Security Industry Awards 2023",
           desc: "MotionCam (PhOD) y MotionCam Outdoor (PhOD) ganan los Australian Security Industry Awards 2023",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FASIAL_Awards_2023_preview_2x_7478b4dbc4%401.png&1714864570",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FASIAL_Awards_2023_preview_2x_7478b4dbc4%401.webp&1714864570",
           href: "https://ajax.systems/es/blog/australian-security-industry-awards-2023/",
         },
       ],
@@ -167,13 +167,13 @@ export const translations = {
         {
           title: "Smart alarms",
           desc: "Accurate intruder detection with sensors and real-time alerts.",
-          srcLight: "/srv/DevicesBlack.png",
-          srcDark: "/srv/DevicesWhite.png"
+          srcLight: "/srv/DevicesBlack.webp",
+          srcDark: "/srv/DevicesWhite.webp"
         },
         {
           title: "HD video surveillance",
           desc: "Clear images, night vision, and remote access from anywhere.",
-          src: "/srv/Bulletcam.png"
+          src: "/srv/Bulletcam.webp"
         },
         {
           title: "Instant alerts",
@@ -183,7 +183,7 @@ export const translations = {
         {
           title: "Access control",
           desc: "Manage who enters and leaves with secure and simple solutions.",
-          src: "/srv/acces.jpg"
+          src: "/srv/acces.webp"
         }
       ],
       more: "Learn more"
@@ -215,8 +215,8 @@ export const translations = {
         "Tailored to your needs",
         "Continuous technical support"
       ],
-      srcLight: "/images/DistB.png",
-      srcDark: "/images/Dist.png"
+      srcLight: "/images/DistB.webp",
+      srcDark: "/images/Dist.webp"
     },
     testimonials: {
       title: "Testimonials",
@@ -225,12 +225,12 @@ export const translations = {
         {
           author: "Carlos M.",
           text: "With Ajax I feel safe; I can monitor my business from my phone.",
-          src: "/inst/anexo4.jpg"
+          src: "/inst/anexo4.webp"
         },
         {
           author: "Laura G.",
           text: "They installed it the same day, excellent service!",
-          src: "/inst/anexo1.jpg"
+          src: "/inst/anexo1.webp"
         }
       ]
     },
@@ -254,25 +254,25 @@ export const translations = {
         {
           title: "Red Dot Winner 2024",
           desc: "Ajax Systems wins its first Red Dot Award for three security and smart home products",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2Freddot_switch_ab76b6f4e3%401.png&1715325401",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2Freddot_switch_ab76b6f4e3%401.webp&1715325401",
           href: "https://ajax.systems/en/blog/ajax-systems-won-red-dot-award/"
         },
         {
           title: "Security Awards 2024",
           desc: "Ajax Systems wins the 2024 Security Awards as Best Smart Home Security Equipment Manufacturer",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FCorporate_Vision_2024_preview_2x_462e208450%401.jpg&1714866025",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FCorporate_Vision_2024_preview_2x_462e208450%401.webp&1714866025",
           href: "https://ajax.systems/en/blog/security-awards-2024/"
         },
         {
           title: "GIT Security Award 2024",
           desc: "FireProtect 2 recognized with the prestigious GIT Security Award 2024",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FWGIT_Security_awards_2023_preview_2x_7a943fadfd%401.png&1714864288",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FWGIT_Security_awards_2023_preview_2x_7a943fadfd%401.webp&1714864288",
           href: "https://ajax.systems/en/blog/fireprotect-2-security-award-2024/"
         },
         {
           title: "Australian Security Industry Awards 2023",
           desc: "MotionCam (PhOD) and MotionCam Outdoor (PhOD) win the 2023 Australian Security Industry Awards",
-          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FASIAL_Awards_2023_preview_2x_7478b4dbc4%401.png&1714864570",
+          src: "https://ajax.systems/api/cdn-img/?img=%2Fupload%2FASIAL_Awards_2023_preview_2x_7478b4dbc4%401.webp&1714864570",
           href: "https://ajax.systems/en/blog/australian-security-industry-awards-2023/"
         }
       ],
