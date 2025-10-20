ccff81dd3e8c5ce880fc5dd6a79aa999e4252e6e
Ulises Cabañas Manrrique <ulicama@hotmail.com>
2025-10-20 17:48:33 -0600

    crenado direccion landing

--- src/app/landing/page.jsx
+++ src/app/landing/page.jsx
@@ -2,32 +2,13 @@
 
 import Link from "next/link";
 import Image from "next/image";
-// import { NextSeo } from "next-seo";
+import NextSeo from "next-seo";
 
-export default function PromoPage() {
+export default function LandingPage() {
   return (
     <>
-      {/* <NextSeo
-        title="Instalación de alarmas Ajax en 24 horas | Protección Total"
-        description="Oferta limitada: instalación de sistemas de alarma Ajax gratis y en 24 horas. Protege tu casa hoy con Protección Total."
-        canonical="https://promo.protecciontotal.pro/"
-        openGraph={{
-          url: "https://promo.protecciontotal.pro/",
-          title: "Protección Total - Instala tu alarma Ajax en 24 horas",
-          description:
-            "Protección Total ofrece instalación gratuita de sistemas Ajax por tiempo limitado. Seguridad sin cables y sin complicaciones.",
-          images: [
-            {
-              url: "https://www.protecciontotal.pro/preview.jpg",
-              width: 1200,
-              height: 630,
-              alt: "Sistema de alarma Ajax instalado por Protección Total",
-            },
-          ],
-        }}
-      /> */}
 
-      <main className="min-h-screen flex flex-col items-center justify-center bg-slate-900 text-white text-center p-8">
+      <main className="min-h-screen flex flex-col items-center justify-center bg text-white text-center px-8 py-2">
         <Image
           src="/images/ajax-offer.png"
           alt="Oferta Ajax Protección Total"
@@ -37,26 +18,29 @@ export default function PromoPage() {
         />
 
         <h1 className="text-4xl font-bold mb-4">
-          🚨 Instalación Gratis + en 24 Horas 🚨
+          🚨 Instalación Gratis en menos de 24 Horas* 🚨
         </h1>
         <p className="text-lg mb-6 max-w-2xl">
           Sistema de alarma <b>Ajax</b> con monitoreo profesional. Ideal para
           hogares, oficinas y negocios. ¡Aprovecha la promoción por tiempo
           limitado!
         </p>
+       
 
         <Link
-          href="https://api.whatsapp.com/send/?phone=524426298541&text=Quiero%20mi%20alarma%20Ajax%20gratis%20en%2024h"
+          href="https://api.whatsapp.com/send/?phone=524426298541&text=Quiero%20mi%20alarma%20Ajax%20en%2024h"
           className="bg-[#83c040] text-slate-900 px-6 py-3 rounded-xl font-semibold hover:bg-[#9adb58] transition-all"
         >
           Solicitar instalación
         </Link>
-
-        <footer className="mt-10 text-sm text-gray-400">
+        
+        <p className=" p-3 gap-3 text-sm">* Aplican restricciones</p>
+        <footer className=" text-sm text-gray-400">
           © {new Date().getFullYear()} Protección Total — Todos los derechos
           reservados.
         </footer>
       </main>
     </>
   );
+  
 }
